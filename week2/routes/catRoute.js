073a5b9c929ff4c1625f5aba53a4db4d1b751bb2
sarao <saraok@metropolia.fi>
2022-11-01 23:43:23 +0200

    week2 first

---
+++ week2/routes/catRoute.js
@@ -0,0 +1,24 @@
+'use strict';
+// catRoute
+const express = require('express');
+const {cat_list_get, cat_get} = require('../controllers/catController');
+const router = express.Router();
+
+router.get('/', cat_list_get);
+
+router.get('/:id', cat_get);
+
+router.post('/', (req, res) => {
+  res.send('With this endpoint you can add cats.')
+});
+
+router.put('/c', (req, res) => {
+  res.send('With this endpoint you can edit cats')
+});
+
+router.delete('/', (req, res) => {
+  res.send('With this endpoint you can delete cats.')
+});
+
+module.exports = router;
+
