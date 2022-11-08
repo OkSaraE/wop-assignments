2f65fd246f9564582a310ea28e594c8392c9b037
sarao <saraok@metropolia.fi>
2022-11-08 10:57:07 +0200

    virheenhallintaa

--- week2/app.js
+++ week2/app.js
@@ -1,19 +1,31 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const userRoute = require('./routes/userRoute');
-const catRoute = require('./routes/catRoute');
+const userRoute = require("./routes/userRoute");
+const catRoute = require("./routes/catRoute");
+const { httpError } = require("./utils/errors");
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(express.static('uploads'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static("uploads"));
 
-app.use('/user', userRoute);
-app.use('/cat', catRoute);
+app.use("/user", userRoute);
+app.use("/cat", catRoute);
+
+app.use((req, res, next)=>{
+  const err = httpError('Not found', 404);
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal server error" });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
