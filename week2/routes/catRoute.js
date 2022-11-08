8a4295b52e45e6e6b722161762dbdddd32ea84b7
sarao <saraok@metropolia.fi>
2022-11-08 10:04:11 +0200

    group

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,20 +1,23 @@
-'use strict';
+"use strict";
 // catRoute
-const express = require('express');
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
-const {cat_list_get, cat_get, cat_post, cat_update_put, cat_delete} = require('../controllers/catController');
+const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const {
+  cat_list_get,
+  cat_get,
+  cat_post,
+  cat_update_put,
+  cat_delete,
+} = require("../controllers/catController");
 const router = express.Router();
 
-router.get('/', cat_list_get);
+router
+  .route("/")
+  .get(cat_list_get)
+  .post(upload.single("cat"), cat_post)
+  .put(cat_update_put);
 
-router.get('/:id', cat_get);
-
-router.post('/', upload.single('cat'), cat_post);
-
-router.put('/', cat_update_put);
-
-router.delete('/:id', cat_delete);
+router.route("/:id").get(cat_get).delete(cat_delete);
 
 module.exports = router;
-
--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -5,11 +5,9 @@ const express = require('express');
 const {user_list_get, user_get, user_create_post} = require('../controllers/userController');
 const router = express.Router();
 
-router.get('/', user_list_get);
+router.route('/').get(user_list_get).post(user_create_post);
 
 router.get('/:id', user_get);
-
-router.post('/', user_create_post)
 
 router.put('/c', (req, res) => {
   res.send('With this endpoint you can edit users')
