bb4fdf1ef11cd86fc5d877c6d9a13e8100ccdf24
sarao <saraok@metropolia.fi>
2022-11-10 09:11:10 +0200

    viimenen

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -1,6 +1,12 @@
-'use strict';
+"use strict";
 // catController
-const {getCat, getAllCats, addCat, updateCat, deleteCat} = require('../models/catModel');
+const {
+  getCat,
+  getAllCats,
+  addCat,
+  updateCat,
+  deleteCat,
+} = require("../models/catModel");
 
 const cat_list_get = async (req, res, next) => {
   const kissat = await getAllCats(next);
@@ -9,66 +15,75 @@ const cat_list_get = async (req, res, next) => {
 
 const cat_get = async (req, res, next) => {
   const cat = await getCat(req.params.id, next);
-  if(cat.length > 0) {
+  if (cat.length > 0) {
     res.json(cat.pop());
   } else {
-    res.send('virhe');
+    res.send("virhe");
   }
 };
 //Add cat
-const cat_post = async (req, res) => {
-  console.log('cat_post', req.body, req.file);
-  const data = [
+const cat_post = async (req, res, next) => {
+  try {
+    console.log("cat_post", req.body, req.file);
+    const data = [
       req.body.name,
       req.body.birthdate,
       req.body.weight,
       req.body.owner,
       req.file.filename,
-  ];
-  const result = await addCat(data);
-  if(result.affectedRows > 0) {
-    res.json({
-      message: 'cat added',
-      cat_id: result.insertId,
-    });
-  } else {
-    res.send('virhe')
+    ];
+    const result = await addCat(data, next);
+    if (result.affectedRows > 0) {
+      res.json({
+        message: "cat added",
+        cat_id: result.insertId,
+      });
+    } else {
+      res.send("virhe");
+    }
+  } catch (e) {
+    console.error("cat_put", e.message);
+    next(httpError("Invalid input", 400));
   }
 };
 
 //Modify cat
-const cat_update_put = async (req, res) => {
-  console.log('cat_update', req.body);
-  const data = [
+const cat_update_put = async (req, res, next) => {
+  try {
+    console.log("cat_update", req.body);
+    const data = [
       req.body.name,
       req.body.birthdate,
       req.body.weight,
       req.body.owner,
       req.body.id,
-      
-  ];
+    ];
 
-  const result = await updateCat(data);
-  if(result.affectedRows > 0) {
-  res.json({
-    message: 'cat updated',
-    cat_id: result.insertId,
-  });
-  } else {
-  res.send('error')
+    const result = await updateCat(data, next);
+    if (result.affectedRows > 0) {
+      res.json({
+        message: "cat modified",
+        cat_id: result.insertId,
+      });
+    } else {
+      res.send("error");
+    }
+  } catch (e) {
+    console.error("cat_put", e.message);
+    next(httpError("Invalid input", 400));
   }
 };
 
 //delete cat
-const cat_delete = async (req, res) => {
-  const result = await deleteCat(req.params.id);
-  if(result.affectedRows > 0) {
+const cat_delete = async (req, res, next) => {
+  const result = await deleteCat(req.params.id, next);
+  if (result.affectedRows > 0) {
     res.json({
-      message: 'cat deleted',
+      message: "cat deleted",
       cat_id: result.insertId,
     });
   } else {
-    res.send('error')
+    res.send("error");
   }
 };
 
